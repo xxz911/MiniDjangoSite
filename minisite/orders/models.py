db1153ad532f9903705be9142d11b24cb4113208
xxz <xxz911@rambler.ru>
2022-10-23 19:16:11 +0300

    Create app "products" and set up model relations (Many-to-Many and One-to-Many)

--- minisite/orders/models.py
+++ minisite/orders/models.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from products.models import Product
 
 
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     decription = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    products = models.ManyToManyField(Product)
