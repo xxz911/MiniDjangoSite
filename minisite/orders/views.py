23e0d849b9b4540a86c5a592b0fe61b2bd511493
xxz <xxz911@rambler.ru>
2022-10-24 19:10:00 +0300

    Created API View on Django REST

--- minisite/orders/views.py
+++ minisite/orders/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
 from orders.models import SalesOrder
+from orders.serializers import OrderSerializer
 
 
 def orders_page(request):
     return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+
+
+class OrderView(ModelViewSet):
+    queryset = SalesOrder.objects.all()
+    serializer_class = OrderSerializer
