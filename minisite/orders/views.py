61bd5d04e65be6a97d2a3814aa09ac3eca3ae56a
xxz <xxz911@rambler.ru>
2022-10-24 18:45:23 +0300

    Added HTML rendering View

--- minisite/orders/views.py
+++ minisite/orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from orders.models import SalesOrder
+
+
+def orders_page(request):
+    return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
