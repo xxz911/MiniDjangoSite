a14bf14b64c3fd7f19852b9eaa1d1cdd68bcfeaf
xxz <xxz911@rambler.ru>
2022-10-26 01:54:36 +0300

    Create frontend client on vue.js

--- minisite/minisite/urls.py
+++ minisite/minisite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from orders.views import orders_page, OrderView
+from orders.views import orders_page, OrderView, orders_app
 
 router = SimpleRouter()
 
@@ -26,6 +26,7 @@ router.register('api/orders', OrderView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', orders_page),
+    path('orders_page', orders_app),
 ]
 
 urlpatterns += router.urls
--- minisite/orders/views.py
+++ minisite/orders/views.py
@@ -12,3 +12,7 @@ def orders_page(request):
 class OrderView(ModelViewSet):
     queryset = SalesOrder.objects.all()
     serializer_class = OrderSerializer
+
+
+def orders_app(request):
+    return render(request, 'main_app.html')
